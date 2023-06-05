33fe3da80b0b6d6d9fa6dadb7eb59e744f6773b0
BrianNkwera <briansimon487@yahoo.com>
2023-06-05 14:09:17 +0300

    format document with prettier

--- src/views/home_view/components/TodoList.vue
+++ src/views/home_view/components/TodoList.vue
@@ -18,7 +18,7 @@ const { updateTodo, deleteTodo } = useTodoStore();
 const props = defineProps<{
   todos: TodoType[];
   loadingTodos: boolean;
-}>()
+}>();
 
 //data
 const loadingDelete = ref(false);
@@ -27,7 +27,6 @@ const confirmationModalOpened = ref(false);
 
 //emits
 const emit = defineEmits(["editTodo"]);
-
 
 //methods
 const checkedCompleted = (todo: TodoType) => {
@@ -83,14 +82,29 @@ const openConfirmationModal = async (id: string) => {
       <h3>List Empty</h3>
     </div>
     <div v-else>
-      <ToDoItem class="mb-3" :key="todo.id" v-for="todo in props.todos" :id="todo.id" :title="todo.title"
-        :completed="todo.completed" :description="todo.description" :created="todo.created"
-        :lastUpdated="todo.lastUpdated" @checkedCompleted="checkedCompleted(todo)"
-        @deleteTodo="openConfirmationModal(todo.id)" @editTodo="editTodo(todo)" />
+      <ToDoItem
+        class="mb-3"
+        :key="todo.id"
+        v-for="todo in props.todos"
+        :id="todo.id"
+        :title="todo.title"
+        :completed="todo.completed"
+        :description="todo.description"
+        :created="todo.created"
+        :lastUpdated="todo.lastUpdated"
+        @checkedCompleted="checkedCompleted(todo)"
+        @deleteTodo="openConfirmationModal(todo.id)"
+        @editTodo="editTodo(todo)"
+      />
     </div>
   </div>
   <ConfirmationModal placeholder="Are you sure you want to delete todo item?">
-    <button style="width: 70px" @click="onDeleteTodo" class="btn bg-secondary text-white" type="button">
+    <button
+      style="width: 70px"
+      @click="onDeleteTodo"
+      class="btn bg-secondary text-white"
+      type="button"
+    >
       <div v-if="loadingDelete" class="spinner-border" role="status">
         <span class="visually-hidden">Loading...</span>
       </div>

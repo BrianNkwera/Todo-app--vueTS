dcdd3c3e16d852c015479c06c54cb5da4845d436
BrianNkwera <briansimon487@yahoo.com>
2023-06-06 12:41:54 +0300

    add notification store

---
+++ src/stores/notificationStore.ts
@@ -0,0 +1,28 @@
+import { defineStore } from "pinia";
+import type { NotificationType } from "../types/notificationInterface";
+
+export default defineStore("notification", {
+  state: () => ({
+    notification: {
+        title: "",
+        details: "",
+        isSuccess: false,
+    } as NotificationType ,
+  }),
+
+  actions: {
+    showNotification(notification: NotificationType) {
+      this.notification.title = notification.title;
+      this.notification.details = notification?.details || "";
+      this.notification.isSuccess = notification.isSuccess;
+
+
+      const toastLiveExample = document.getElementById("notificationToast");
+
+      //@ts-ignore
+      const toast = new bootstrap.Toast(toastLiveExample);
+
+      toast.show();
+    },
+  },
+});

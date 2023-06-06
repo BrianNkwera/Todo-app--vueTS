56c8007119dc1bcdc732f9e403e574ffaf438e40
BrianNkwera <briansimon487@yahoo.com>
2023-06-06 12:45:32 +0300

    add hide notification

--- src/stores/notificationStore.ts
+++ src/stores/notificationStore.ts
@@ -4,10 +4,10 @@ import type { NotificationType } from "../types/notificationInterface";
 export default defineStore("notification", {
   state: () => ({
     notification: {
-        title: "",
-        details: "",
-        isSuccess: false,
-    } as NotificationType ,
+      title: "",
+      details: "",
+      isSuccess: false,
+    } as NotificationType,
   }),
 
   actions: {
@@ -16,13 +16,17 @@ export default defineStore("notification", {
       this.notification.details = notification?.details || "";
       this.notification.isSuccess = notification.isSuccess;
 
-
       const toastLiveExample = document.getElementById("notificationToast");
 
       //@ts-ignore
       const toast = new bootstrap.Toast(toastLiveExample);
 
       toast.show();
+    },
+    closeNotification() {
+      this.notification.title = "";
+      this.notification.details = "";
+      this.notification.isSuccess = false;
     },
   },
 });

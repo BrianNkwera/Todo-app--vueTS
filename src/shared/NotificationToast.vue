05d23614bd3e800d2a1f6df6af3e34a907be7e59
BrianNkwera <briansimon487@yahoo.com>
2023-06-06 12:56:04 +0300

    add bootstratp notification toast

--- src/shared/NotificationToast.vue
+++ src/shared/NotificationToast.vue
@@ -1,48 +1,41 @@
 <template>
-  <v-snackbar
-    style="z-index: 5000"
-    v-model="notification.show"
-    color="white"
-    :elevation="24"
-    :light="true"
-    :timeout="5000"
-    :top="true"
-  >
-    <div class="toast-header mb-2">
-      <strong class="me-auto">
-        <font-awesome-icon
-          v-if="!notification.isSuccess"
-          icon="fa-solid fa-circle-exclamation"
-          class="text-danger me-2"
-        />
-        <font-awesome-icon
-          v-else
-          icon="fa-solid fa-circle-check"
-          class="text-success me-2"
-        />
-        {{ notification.title }}
-      </strong>
-      <button
-        type="button"
-        class="btn-close"
-        data-bs-dismiss="toast"
-        aria-label="Close"
-        @click="onCloseNotification"
-      ></button>
+  <div class="toast-container position-fixed bottom-0 end-0 p-3">
+    <div
+      id="notificationToast"
+      class="toast"
+      role="alert"
+      aria-live="assertive"
+      aria-atomic="true"
+    >
+      <div class="toast-header">
+        <strong class="me-auto">
+          <font-awesome-icon
+            v-if="!notification.isSuccess"
+            icon="fa-solid fa-circle-exclamation"
+            class="text-danger me-2"
+          />
+          <font-awesome-icon
+            v-else
+            icon="fa-solid fa-circle-check"
+            class="text-success me-2"
+          />
+          {{ notification.title }}
+        </strong>
+        <button
+          type="button"
+          class="btn-close"
+          data-bs-dismiss="toast"
+          aria-label="Close"
+        ></button>
+      </div>
+      <div class="toast-body bg-white">{{ notification.details }}</div>
     </div>
-    <div class="toast-body bg-white">{{ notification.details }}</div>
-  </v-snackbar>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { storeToRefs } from "pinia";
 import useNotificationStore from "../stores/notificationStore";
+import { storeToRefs } from "pinia";
 
-const { closeNotification } = useNotificationStore();
 const { notification } = storeToRefs(useNotificationStore());
-
-//methods
-const onCloseNotification = () => {
-  closeNotification();
-};
 </script>

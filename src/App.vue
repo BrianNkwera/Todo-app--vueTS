a7a61e9e9b96b16eda35a8fc19cb8bedb980f7e4
BrianNkwera <briansimon487@yahoo.com>
2023-06-06 12:58:11 +0300

    add Notification Toast toapp

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import HomeView from './views/home_view/HomeView.vue';
-
+import HomeView from "./views/home_view/HomeView.vue";
+import NotificationToast from "./shared/NotificationToast.vue";
 </script>
 
 <template>
   <div id="app">
     <HomeView />
+    <NotificationToast />
   </div>
 </template>
 

70e0db5d592a084c22c86c415b3cf345ae09a7e0
BrianNkwera <briansimon487@yahoo.com>
2023-06-07 12:26:18 +0300

    add offline view when page is offline, not connected to the internet

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <script setup lang="ts">
+import { ref, onMounted } from "vue";
+
 import HomeView from "./views/home_view/HomeView.vue";
 import NotificationToast from "./shared/NotificationToast.vue";
 import NoInternet from "./shared/NoInternet.vue";
+
+const isOnline = ref(navigator.onLine);
+
+onMounted(() => {
+  window.addEventListener("offline", () => {
+    isOnline.value = false;
+  });
+
+  window.addEventListener("online", () => {
+    isOnline.value = true;
+  });
+});
 </script>
 
 <template>
   <div id="app">
-    <HomeView />
-    <NoInternet />
-    <NotificationToast />
+    <div v-if="isOnline">
+      <HomeView />
+      <NotificationToast />
+    </div>
+    <NoInternet v-else/>
   </div>
 </template>
 
--- src/shared/NoInternet.vue
+++ src/shared/NoInternet.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="d-flex align-items-center">
-    <div class="bg-primary text-white">
-      <font-awesome-icon icon="fa-solid fa-wifi" />
+    <div class="bg-primary text-white text-center mx-auto p-5 rounded-5">
+      <div class="position-relative">
+        <font-awesome-icon icon="fa-solid fa-wifi" class="wifi" />
+        <font-awesome-icon
+          icon="fa-solid fa-circle-xmark"
+          class="position-absolute top-50 start-100 translate-middle cross"
+        />
+      </div>
     </div>
   </div>
 </template>
-<script setup lang="ts"></script>
-<style scoped></style>
+<script setup lang="ts">
+</script>
+<style scoped>
+.wifi {
+  font-size: 10rem;
+}
+.cross {
+    font-size: 5rem;
+}
+</style>

e43c9042aa9a06c264ba85d40fc193c1aa6ea2af
BrianNkwera <briansimon487@yahoo.com>
2023-06-07 12:33:15 +0300

    add Logo component

---
+++ src/shared/Logo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="mb-4">
+    <div>
+      <h3 class="text-center text-primary mt-3">
+        <font-awesome-icon icon="fa-solid fa-list-check" /> To Do List
+      </h3>
+    </div>
+  </div>
+</template>
+<script setup>
+</script>

73080cebcfc9a8effb01d4d6345081d97ffd2e99
BrianNkwera <briansimon487@yahoo.com>
2023-06-07 12:01:44 +0300

    add no internet icon

---
+++ src/shared/NoInternet.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="d-flex align-items-center">
+    <div class="bg-primary text-white">
+      <font-awesome-icon icon="fa-solid fa-wifi-exclamation" />
+    </div>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style scoped></style>
